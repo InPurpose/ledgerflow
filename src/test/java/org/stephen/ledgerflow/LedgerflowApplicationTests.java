e64b82c4dfb31244c79bc79939436f6b45750f45
Stephen Huang <57771700+InPurpose@users.noreply.github.com>
2026-08-25 16:01:40 -0700

    Initialize Spring Boot ledgerflow project

---
+++ src/test/java/org/stephen/ledgerflow/LedgerflowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stephen.ledgerflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
